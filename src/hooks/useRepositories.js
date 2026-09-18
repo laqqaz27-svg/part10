e44709424088555c6a02d8b5f4dc1d406ae366f3
Student <student@sirafrica.org>
2026-09-18 16:24:28 +0300

    11. Fetching repositories with Apollo Client

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,20 @@
+import { useQuery } from '@apollo/client/react';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, loading, error, refetch } = useQuery(
+    GET_REPOSITORIES,
+    {
+      fetchPolicy: 'cache-and-network',
+    },
+  );
+
+  return {
+    repositories: data ? data.repositories : undefined,
+    loading,
+    error,
+    refetch,
+  };
+};
+
+export default useRepositories;
