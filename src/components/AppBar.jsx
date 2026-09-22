fac8dd472aebd648caad3c5db2138bef604c991f
Student <student@sirafrica.org>
2026-09-22 10:24:21 +0300

    16. Sign out

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,7 +1,20 @@
 import { View, StyleSheet, Pressable, ScrollView } from 'react-native';
 import { Link } from 'react-router-native';
 import Constants from 'expo-constants';
+import { useApolloClient, useQuery } from '@apollo/client/react';
+import { gql } from '@apollo/client';
+
 import Text from './Text';
+import useAuthStorage from '../hooks/useAuthStorage';
+
+const ME = gql`
+  query {
+    me {
+      id
+      username
+    }
+  }
+`;
 
 const styles = StyleSheet.create({
   container: {
@@ -19,6 +32,15 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const { data } = useQuery(ME);
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+
+  const handleSignOut = async () => {
+    await authStorage.removeAccessToken();
+    await apolloClient.resetStore();
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView horizontal>
@@ -26,12 +48,18 @@ const AppBar = () => {
           <Text style={styles.text}>Repositories</Text>
         </Link>
 
-        <Link to="/signin" component={Pressable} style={styles.tab}>
-          <Text style={styles.text}>Sign in</Text>
-        </Link>
+        {data?.me ? (
+          <Pressable onPress={handleSignOut} style={styles.tab}>
+            <Text style={styles.text}>Sign out</Text>
+          </Pressable>
+        ) : (
+          <Link to="/signin" component={Pressable} style={styles.tab}>
+            <Text style={styles.text}>Sign in</Text>
+          </Link>
+        )}
       </ScrollView>
     </View>
   );
-};;
+};
 
 export default AppBar;
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,12 +1,24 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { SetContextLink } from '@apollo/client/link/context';
 
 const httpLink = new HttpLink({
   uri: process.env.EXPO_PUBLIC_APOLLO_URI,
 });
 
-const createApolloClient = () => {
+const createApolloClient = (authStorage) => {
+  const authLink = new SetContextLink(async ({ headers }) => {
+    const accessToken = await authStorage.getAccessToken();
+
+    return {
+      headers: {
+        ...headers,
+        authorization: accessToken ? `Bearer ${accessToken}` : '',
+      },
+    };
+  });
+
   return new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 };
