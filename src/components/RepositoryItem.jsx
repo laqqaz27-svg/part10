5d5dbcf77d1dc159138ec7d09aa3569cd303d32e
Student <student@sirafrica.org>
2026-09-22 11:29:48 +0300

    17. Testing the reviewed repositories list

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -52,7 +52,7 @@ const formatCount = (count) => {
 
 const RepositoryItem = ({ item }) => {
   return (
-    <View style={styles.container}>
+  <View style={styles.container} testID="repositoryItem">
       <View style={styles.header}>
         <Image
           style={styles.avatar}
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,7 @@
 import { FlatList, View, StyleSheet } from 'react-native';
+
 import RepositoryItem from './RepositoryItem';
+
 import useRepositories from '../hooks/useRepositories';
 
 const styles = StyleSheet.create({
@@ -10,9 +12,7 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
-
+export const RepositoryListContainer = ({ repositories }) => {
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : [];
@@ -24,6 +24,12 @@ const RepositoryList = () => {
       renderItem={({ item }) => <RepositoryItem item={item} />}
     />
   );
+};
+
+const RepositoryList = () => {
+  const { repositories } = useRepositories();
+
+  return <RepositoryListContainer repositories={repositories} />;
 };
 
 export default RepositoryList;
