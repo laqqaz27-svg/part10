1271f039c0a4a519df9ce7c5f317099a3a6ad3ae
Student <student@sirafrica.org>
2026-09-25 11:33:54 +0300

    28. Publishing the app via EAS Publish

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rate Repository App
+
+Scan the QR code with Expo Go to open the published application:
+
+![Expo QR code](./assets/expo-qr-code.png)
+
+[EAS update dashboard](https://expo.dev/accounts/gatluak/projects/rate-repository-app/updates/18cc812d-f76f-43a6-ba13-aae2f25d2ef5)
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -40,3 +40,68 @@ export const GET_REPOSITORIES = gql`
     }
   }
 `;
+
+export const GET_REPOSITORY = gql`
+  query GetRepository(
+    $repositoryId: ID!
+    $first: Int
+    $after: String
+  ) {
+    repository(id: $repositoryId) {
+      id
+      fullName
+      description
+      language
+      forksCount
+      stargazersCount
+      reviewCount
+      ratingAverage
+      ownerAvatarUrl
+      url
+      reviews(first: $first, after: $after) {
+        totalCount
+        edges {
+          node {
+            id
+            text
+            rating
+            createdAt
+            user {
+              id
+              username
+            }
+          }
+          cursor
+        }
+        pageInfo {
+          endCursor
+          startCursor
+          hasNextPage
+        }
+      }
+    }
+  }
+`;
+
+export const CREATE_REVIEW = gql`
+  mutation CreateReview($review: CreateReviewInput!) {
+    createReview(review: $review) {
+      repositoryId
+    }
+  }
+`;
+
+export const CREATE_USER = gql`
+  mutation CreateUser($user: CreateUserInput!) {
+    createUser(user: $user) {
+      id
+      username
+    }
+  }
+`;
+
+export const DELETE_REVIEW = gql`
+  mutation DeleteReview($id: ID!) {
+    deleteReview(id: $id)
+  }
+`;
